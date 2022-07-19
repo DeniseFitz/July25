50758d5e5d8be86146cd9785067fff0d12163124
Denise Poll <denise.poll@seo-usa.org>
2022-07-19 22:14:10 +0000

    db updates

--- demo.py
+++ demo.py
@@ -1,49 +1,60 @@
 from flask import Flask, render_template, url_for, flash, redirect # allow rendering of html code rather than printing it raw
 from flask_wtf import FlaskForm
+from flask_behind_proxy import FlaskBehindProxy ## add this line to imports
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 #import secrets
-from forms import RegistrationForm
+from forms import RegistrationForm, LoginForm
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
 
+'''
+Once you populate you can query the database from the terminal, 
+notice "demo" is the name of my python file
+
+python3
+from demo import usertable
+usertable.query.all()  # check the database
+'''
 
 app = Flask(__name__)                    # this gets the name of the file so Flask knows it's name
+proxied = FlaskBehindProxy(app) 
 app.config['SECRET_KEY'] = 'fb93246348ed383a9de5b7e77ff8d579' # be sure to use only the most recent key generated
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-
+#name of the database to create or use
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site2.db'
 
 db = SQLAlchemy(app)
 
-class User(db.Model):
+#Creates a table in the database named 'usertable"
+class usertable(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String(20), unique=True, nullable=False)
   email = db.Column(db.String(120), unique=True, nullable=False)
   password = db.Column(db.String(60), nullable=False)
-  
+
   def __repr__(self):
-    return f"User('{self.username}', '{self.email}')"
+    return f"usertable('{self.username}', '{self.email}', '{self.password}')"
+#new function to make sure table is created in the database when needed 
+def __init__(self, username, email, password):
+   self.username = username
+   self.email = email
+   self.password = password
 
-@app.route("/")                          # this tells you the URL the method below is related to
-def hello_world():
-    return "<p>Hello, SEO Tech Developers!</p>"        # this prints HTML to the webpage
-
-# # Let’s add an About page to our simple website.
-# # First, pick the URL you want the page to be at – for example, website.com/about is pretty standard:
-# @app.route("/about")
-# def about():
-#     return "<p>About SEO!</p>"
+#new code added to make sure table is created
+db.create_all()
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
-        db.session.add(user)
+        user_a = usertable(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(user_a)
         db.session.commit()
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
+@app.route("/")  
 @app.route("/home")
 def home():
     return render_template('home.html', subtitle='Home Page')
@@ -52,7 +63,10 @@ def home():
 def about():
     return render_template('about.html', subtitle='About Page')
 
-
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', form=form)
 
 
 if __name__ == '__main__':               # this should always be at the end avoids the need for environment variables
